dca58dcf317f19a1d7d221e50b8157d4b3bf4a0a
markfelixm <31193782+Markfelixm@users.noreply.github.com>
2022-06-01 13:41:10 +0300

    update description

--- Metaflow/Flows/parameterflow.py
+++ Metaflow/Flows/parameterflow.py
@@ -8,8 +8,13 @@
 # If no default value and no value provided, given None.
 # Parameters may be set as required and can be passed into the flow via terminal env variables.
 #
-# Use parameter name to interact from command line.
-# e.g. python parameterflow.py run --pi 3.14159
+# Use parameter name as an option to interact from command line.
+# ~ python parameterflow.py run --pi 3.14159
+# Shell environment variables may be prefixed with METAFLOW_RUN_
+# This will overwrite command line option
+# ~ export METAFLOW_RUN_PI=3.14159265
+# ~ python parameterflow.py run --pi 3.14
+# pi will have value 3.14159265
 
 from metaflow import FlowSpec, step, Parameter
 
